b092087b6b651efee82740b3351a0d72f98582fc
EnderDev <xenderpugx@gmail.com>
2020-07-19 11:20:53 +0100

    ✨ name thing

--- pizza.cpp
+++ pizza.cpp
@@ -2,12 +2,17 @@
 
 using std::cout;
 using std::cin;
+using std::string;
 
 int main() {
+	string name = "MONKY";
 	int slices = 15;
+
+	cout << "What is your name? (default: monky): ";
+	cin >> name;
 
 	cout << "How many slices of pizza do you want?: ";
 	cin >> slices;
 
-	cout << "You have " << slices << " slices of pizza" << std::endl;
+	cout << name << ", you have " << slices << " slices of pizza" << std::endl;
 }
