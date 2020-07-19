303f6df0c0739fead42d89dcf8485b6229b2d944
EnderDev <xenderpugx@gmail.com>
2020-07-19 15:37:02 +0100

    age

---
+++ game/index.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int age = 17;
+
+    if(age < 13) {
+        cout << "You are not old enough!";
+    } else {
+        cout << "Welcome";
+    }
+}
