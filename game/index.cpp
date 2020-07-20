d7c75787296ac2ce275183d6041261c17ae70e0c
EnderDev <xenderpugx@gmail.com>
2020-07-20 14:42:03 +0100

    game

--- game/index.cpp
+++ game/index.cpp
@@ -8,30 +8,27 @@
 using namespace std;
 
 void play();
+void showTitleScreen();
 
 int main() {
     srand(time(NULL));
 
     int option = 0;
 
+    cout << endl << "\tGuessing game" << endl << endl;
+    cout << "\t1. Play Game" << endl;
+    cout << "\t2. Quit Game" << endl;
+    cout << endl;
+
     do {
-        cout << "\tGuessing game" << endl << endl;
-        cout << "\t1. Play Game" << endl;
-        cout << "\t2. Quit Game" << endl;
-        cout << endl;
-        cout << "=> ";
+        showTitleScreen();
 
         cin >> option;
 
-        switch(option) {
-            case 1:
-                play();
-                return 0;
-            case 2:
-                cout << "Goodbye :(" << endl;
-                break;
-            default:
-                break;
+        if(option == 1) {
+            play();
+        } else if(option == 2) {
+            return 0;
         }
 
     } while(option != 2);
@@ -39,14 +36,28 @@ int main() {
 }
 
 void play() {
-    int random = rand() % 251;
-    
-    cout << "Rolling you a random number..." << endl << "Guess the number" << endl << "=> ";
-
     int guess;
+    int answer = rand() % 51;
+    
+    cout << endl << "Rolling you a random number..." << endl << "Guess the number" << endl << endl;
 
-    cin >> guess;
+    while(guess != answer) {
 
-    if(guess == random) cout << endl << "You got it correct!";
-    else cout << endl << "It was incorrect, the number was " << random;
+        cout << "=> ";
+
+        cin >> guess;
+
+        if(guess == answer) {
+            cout << endl << "You got it correct!" << endl;
+            cout << endl << "Press 1 to restart or 2 to quit." << endl << endl;
+        }
+        else if(guess > answer) cout << endl << "Hint: Go lower!" << endl << endl;
+        else if(guess < answer) cout << endl << "Hint: Go higher!" << endl << endl;
+    }
+
+
+}
+
+void showTitleScreen() {
+    cout << "=> ";
 }
